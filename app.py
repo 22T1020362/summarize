2a0d937cbef752cc6681f5b33848805b6799e6e1
Quang <115056697+22T1020362@users.noreply.github.com>
2023-07-10 14:33:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,115 +78,110 @@ st.write("")
 st.write("Or upload a File...")
 uploaded_file = st.file_uploader("Choose a file")
 
+if uploaded_file is not None:
 
+    #convert pdf to txt
+    path = uploaded_file.read()
+    file_extension = uploaded_file.name.split(".")[-1]
+    file_name = uploaded_file.name.split(".")[0]
+    stringio = ""
+    
+    if file_extension == "pdf":
+        stringio, nbPages = convert_pdf_to_txt_file(uploaded_file)
+        totalPages = "Pages: "+str(nbPages)+" in total"
 
-try:
-    
-    if uploaded_file is not None:
-    
-        #convert pdf to txt
-        path = uploaded_file.read()
-        file_extension = uploaded_file.name.split(".")[-1]
-        file_name = uploaded_file.name.split(".")[0]
+    elif file_extension == "txt":
         stringio = ""
-        
-        if file_extension == "pdf":
-            stringio, nbPages = convert_pdf_to_txt_file(uploaded_file)
-            totalPages = "Pages: "+str(nbPages)+" in total"
-    
-        elif file_extension == "txt":
-            stringio = ""
-    
-        elif file_extension == "csv":
-            stringio = pd.read_csv(path)
-    
-        #audio, mp3 file
-        elif file_extension == "mp3" or file_extension == "wav":
-            if file_extension == "mp3":
-                ex = uploaded_file.name
-                file_export = file_name + "wav"
-                sound = AudioSegment.from_mp3(ex)
-                sound.export(file_export, format="wav")
-                r = sr.Recognizer()
-                with sr.AudioFile(file_export) as source:
-                    audio = r.record(source)  # read the entire audio file                  
-                    stringio = r.recognize_google(audio)
-            else:
-                AUDIO_FILE = uploaded_file.name
-                r = sr.Recognizer()
-                with sr.AudioFile(AUDIO_FILE) as source:
-                    audio = r.record(source)  # read the entire audio file                  
-                    stringio = r.recognize_google(audio)
-                
-        #img,png,jpg
+
+    elif file_extension == "csv":
+        stringio = pd.read_csv(path)
+
+    #audio, mp3 file
+    elif file_extension == "mp3" or file_extension == "wav":
+        if file_extension == "mp3":
+            ex = uploaded_file.name
+            file_export = file_name + "wav"
+            sound = AudioSegment.from_mp3(ex)
+            sound.export(file_export, format="wav")
+            r = sr.Recognizer()
+            with sr.AudioFile(file_export) as source:
+                audio = r.record(source)  # read the entire audio file                  
+                stringio = r.recognize_google(audio)
         else:
-            pil_image = Image.open(uploaded_file)
-            stringio = pytesseract.image_to_string(pil_image)
+            AUDIO_FILE = uploaded_file.name
+            r = sr.Recognizer()
+            with sr.AudioFile(AUDIO_FILE) as source:
+                audio = r.record(source)  # read the entire audio file                  
+                stringio = r.recognize_google(audio)
             
-    
-        string_data = ""
+    #img,png,jpg
+    else:
+        pil_image = Image.open(uploaded_file)
+        stringio = pytesseract.image_to_string(pil_image)
         
-        if stringio == "":
-            stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-            string_data = stringio.read()
-        
-        else:
-            string_data = stringio
-        product = string_data
+
+    string_data = ""
     
-    x = ""
-    check = False
-    resans = ""
-    if st.button("Summarize"):
-        with st.spinner('Wait for it...'):
-            time.sleep(1)
-        if product != "":
-            x = chain.run(product)
-            resans = st.text_area(label="Text after Summarize", value=chain.run(product))
-            check = True
+    if stringio == "":
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+    
+    else:
+        string_data = stringio
+    product = string_data
+
+x = ""
+check = False
+resans = ""
+if st.button("Summarize"):
+    with st.spinner('Wait for it...'):
+        time.sleep(1)
+    if product != "":
+        x = chain.run(product)
+        resans = st.text_area(label="Text after Summarize", value=chain.run(product))
+        check = True
+        
+    else:
+        print("Error! Please write or upload a file")
             
-        else:
-            print("Error! Please write or upload a file")
-                
-    inp = ""
+inp = ""
+
+llm = OpenAI(temperature=0)
+if resans:
+    inp = "Summarize" + product
+    memory.save_context({"input": inp}, 
+                    {"output": x})
+    conversation = ConversationChain(
+        llm=llm, verbose=True, memory=memory
+    )
+    with st.spinner('Wait for it...'):
+        time.sleep(1)
+    st.write("Asking about the context summarize above")
     
-    llm = OpenAI(temperature=0)
-    if resans:
-        inp = "Summarize" + product
-        memory.save_context({"input": inp}, 
-                        {"output": x})
-        conversation = ConversationChain(
-            llm=llm, verbose=True, memory=memory
-        )
-        with st.spinner('Wait for it...'):
-            time.sleep(1)
-        st.write("Asking about the context summarize above")
-        
-        # Layout of input/response containers
-        input_container = st.container()
-        colored_header(label='', description='', color_name='blue-30')
-        response_container = st.container()
-        
-        
-        with input_container:
-            input_text = st.text_input("Question: ", "", key = "input", placeholder="Ask something...")
-            query = input_text
-        with response_container:  
-                
-            if st.button(label="Ask"):
-                with st.spinner("generating..."):
-                    st.session_state.past.append(query)
-                    st.session_state.generated.append(conversation.predict(input=query))
-                    
-                memory.save_context({"input": query}, 
-                    {"output": conversation.predict(input=query)})
-                conversation = ConversationChain(
-                    llm=llm, verbose=True, memory=memory
-                )
+    # Layout of input/response containers
+    input_container = st.container()
+    colored_header(label='', description='', color_name='blue-30')
+    response_container = st.container()
     
-            if st.session_state['generated']:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-                    message(st.session_state['generated'][i], key=str(i))
-except:
-    st.write("Error!!!")
+    
+    with input_container:
+        input_text = st.text_input("Question: ", "", key = "input", placeholder="Ask something...")
+        query = input_text
+    with response_container:  
+            
+        if st.button(label="Ask"):
+            with st.spinner("generating..."):
+                st.session_state.past.append(query)
+                st.session_state.generated.append(conversation.predict(input=query))
+                
+            memory.save_context({"input": query}, 
+                {"output": conversation.predict(input=query)})
+            conversation = ConversationChain(
+                llm=llm, verbose=True, memory=memory
+            )
+
+        if st.session_state['generated']:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state['generated'][i], key=str(i))
+                
